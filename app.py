376ad3c278f6114ae74655366bf671c9b6110139
Akash Acharya <akashacharya17103@gmail.com>
2026-08-01 10:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,8 +108,8 @@ if all(all_API):
   leader_agent = create_agent(
       model=model,
       tools=[
-          search_latest_info,
-          # generate_image])
+          search_latest_info])
+          # generate_image 
 
   #leader_agent
 else:
